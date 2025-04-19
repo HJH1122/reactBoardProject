8385822a19eef2e75e26973c5e0fc137f3e235d4
hjh <ktorn21@naver.com>
2025-04-19 22:14:12 +0900

    업로드 버튼 추가

--- board-front/src/layouts/Header/index.tsx
+++ board-front/src/layouts/Header/index.tsx
@@ -1,18 +1,38 @@
 import React, { ChangeEvent, KeyboardEvent, useEffect, useRef, useState } from 'react'
 import './style.css'
-import { AUTH_PATH, MAIN_PATH, SEARCH_PATH, USER_PATH } from 'constant';
-import { useNavigate, useParams } from 'react-router-dom';
+import { AUTH_PATH, BOARD_DETAIL_PATH, BOARD_PATH, BOARD_UPDATE_PATH, BOARD_WRITE_PATH, MAIN_PATH, SEARCH_PATH, USER_PATH } from 'constant';
+import { useLocation, useNavigate, useParams } from 'react-router-dom';
 import { useCookies } from 'react-cookie';
-import { useLoginUserStore } from 'stores';
+import { useBoardStore, useLoginUserStore } from 'stores';
 
 export default function Header() {
 
   // state: 로그인 유저 상태
   const {loginUser, setLoginUser, resetLoginUser} = useLoginUserStore();
+  // state: path 상태
+  const {pathname} = useLocation();
   // state: cookie 상태
   const [cookies, setCookies] = useCookies();
   // state: 로그인 상태
-  const [isLogin, setIsLogin] = useState<boolean>(false);
+  const [isLogin, setLogin] = useState<boolean>(false);
+  // state: 인증 페이지 상태
+  const [isAuthPage, setAuthPage] = useState<boolean>(false);
+  // state: 메인 페이지 상태
+  const [isMainPage, setMainPage] = useState<boolean>(false);
+  // state: 검색 페이지 상태
+  const [isSearchPage, setSearchPage] = useState<boolean>(false);
+  // state: 게시물 상세 페이지 상태
+  const [isBoardDetailPage, setBoardDetailPage] = useState<boolean>(false);
+  // state: 게시물 작성 페이지 상태
+  const [isBoardWritePage, setBoardWritePage] = useState<boolean>(false);
+  // state: 게시물 수정 페이지 상태
+  const [isBoardUpdatePage, setBoardUpdatePage] = useState<boolean>(false);
+  // state: 유저 페이지 상태
+  const [isUserPage, setUserPage] = useState<boolean>(false);
+
+
+
+
 
 
   // function: 네비게이트 함수
@@ -86,7 +106,7 @@ export default function Header() {
     );
   }
 
-  // component: 로그인 또는 마이페이지 버튼 컴포넌트
+  // component: 마이페이지 버튼 컴포넌트
   const MyPageButton = () =>{
 
     // state: 유저 이메일 path variable 상태
@@ -124,6 +144,47 @@ export default function Header() {
   
   }
 
+  // component: 업로드 버튼 컴포넌트
+  const UploadButton = () =>{
+
+    // state : 게시물 상태 
+    const {title, content, boardImageFileList, resetBoard} = useBoardStore();
+    
+
+    // event handler : 업로드 버튼 클릭 이벤트
+    const onUploadButtonClickHandler = () =>{
+
+    }
+
+    // render: 업로드 버튼 컴포넌트 렌더링
+    if(title && content)
+    return <div className='black-button' onClick={onUploadButtonClickHandler}>{'업로드'}</div>;
+    // render: 업로드 불가 버튼 컴포넌트 렌더링
+
+    return <div className='disable-button'>{'업로드'}</div>;
+
+  }
+
+  // effect: path가 변결될때마다 실행될 함수
+  useEffect(()=>{
+
+    const isAuthPage = pathname.startsWith(AUTH_PATH());
+    setAuthPage(isAuthPage);
+    const isMainPage = pathname === MAIN_PATH();
+    setMainPage(isMainPage);
+    const isSearchPage = pathname.startsWith(SEARCH_PATH(''));
+    setSearchPage(isSearchPage);
+    const isBoardDetailPage = pathname.startsWith(BOARD_PATH() + '/' + BOARD_DETAIL_PATH(''));
+    setBoardDetailPage(isBoardDetailPage);
+    const isBoardWritePage = pathname.startsWith(BOARD_PATH() + '/' + BOARD_WRITE_PATH());
+    setBoardWritePage(isBoardWritePage);
+    const isBoardUpdatePage = pathname.startsWith(BOARD_PATH() + '/' + BOARD_UPDATE_PATH(''));
+    setBoardUpdatePage(isBoardUpdatePage);
+    const isUserPage = pathname.startsWith(USER_PATH(''));
+    setUserPage(isUserPage);
+
+  },[pathname])
+
   return (
     <div id='header'>
       <div className='header-container'>
@@ -134,8 +195,10 @@ export default function Header() {
           <div className='header-logo'>{'HJH Board'}</div>
         </div>
         <div className='header-right-box'>
-          <SearchButton />
-          <MyPageButton />
+          {(isAuthPage || isMainPage || isSearchPage|| isBoardDetailPage) && <SearchButton />}
+          {(isMainPage || isSearchPage || isBoardDetailPage|| isUserPage) && <MyPageButton />}
+          {(isBoardWritePage || isBoardUpdatePage) && <UploadButton />}
+
         </div>
       </div>
     </div>
