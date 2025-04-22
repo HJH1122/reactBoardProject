4674ccd2b60c59c6c58abbddd0f3e26299af95c6
hjh <ktorn21@naver.com>
2025-04-22 17:43:20 +0900

    로그인화면 추가

--- board-front/src/views/Authentication/index.tsx
+++ board-front/src/views/Authentication/index.tsx
@@ -1,13 +1,95 @@
-import React from 'react'
+import React, { useState, KeyboardEvent, useRef } from 'react'
 import './style.css'
+import InputBox from 'components/InputBox';
 
 
 export default function Authentication() {
 
+  const [view, setView] = useState<'sign-in' | 'sign-up'>('sign-in');
+
+  const SignInCard = () =>{
+
+    const emailRef = useRef<HTMLInputElement | null>(null);
+    const passwordRef = useRef<HTMLInputElement | null>(null);
+    const [email, setEmail] = useState<string>('');
+    const [password, setPassword] = useState<string>('');
+    const[passwordType, setPasswordType] = useState<'text' | 'password'>('password');
+    const [passwordButtonIcon, setPasswordButtonIcon] = useState<'eye-light-off-icon' | 'eye-light-on-icon'>('eye-light-off-icon');
+    const [error, setError] = useState<boolean>(false);
+    
+    const onSignInButtonClickHandler = () =>{
+
+    }
+
+    const onPasswordButtonClickHandler = () =>{
+      if(passwordType === 'text'){
+        setPasswordType('password');
+        setPasswordButtonIcon('eye-light-off-icon');
+
+      }
+      else{
+        setPasswordType('text');
+        setPasswordButtonIcon('eye-light-on-icon');
+      }
+    }
+
+    const onEmailKeyDownHandler = (event: KeyboardEvent<HTMLInputElement>) =>{
+      if(event.key !== 'Enter') return;
+      if(!passwordRef.current) return;
+      passwordRef.current.focus();
+
+    }
+
+    const onPasswordKeyDownHandler = (event: KeyboardEvent<HTMLInputElement>) =>{
+      if(event.key !== 'Enter') return;
+      onSignInButtonClickHandler();
+    }
+
+    return(
+      <div className='auth-card'>
+        <div className='auth-box'>
+          <div className='auth-card-top'>
+            <div className='auth-card-title-box'>
+              <div className='auth-card-title'>{'로그인'}</div>
+            </div>
+            <InputBox ref={emailRef} label='이메일 주소' type='text' placeholder='이메일 주소를 입력해주세요.' error={error} value={email} setValue={setEmail} onKeyDown={onEmailKeyDownHandler}/>
+            <InputBox ref={passwordRef} label='패스워드' type={passwordType} placeholder='비밀번호를 입력해주세요.' error={error} value={password} setValue={setPassword} icon={passwordButtonIcon} onButtonClick={onPasswordButtonClickHandler} onKeyDown={onPasswordKeyDownHandler}/>
+          </div>
+          <div className='auth-card-bottom'>
+            <div className='auth-sign-in-error-box'>
+              <div className='auth-sign-in-error-message'>{'이메일 주소 또는 비밀번호를 잘못입력했습니다.'}</div>
+            </div> 
+            <div className='black-large-full-button' onClick={onSignInButtonClickHandler}>{'로그인'}</div>
+            <div className='auth-description-box'>
+              <div className='auth-description'>{'신규 사용자이신가요?'}<span className='auth-description-link'>{'회원가입'}</span></div>
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+  }
+
+  const SignUpCard = () =>{
+    return(
+      <div className='auth-card'></div>
+    )
+  }
 
   return (
-    <div>
-      인증화면
+    <div id='auth-wrapper'>
+      <div className='auth-container'>
+        <div className='auth-jumbotron-box'>
+          <div className='auth-jumbotron-contents'>
+            <div className='auth-logo-icon'></div>
+            <div className='auth-jumbotron-text-box'>
+              <div className='auth-jumbotron-text'>{'환영합니다'}</div>
+              <div className='auth-jumbotron-text'>{'보드게시판입니다'}</div>
+            </div>
+          </div>
+        </div>
+        {view === 'sign-in' && <SignInCard />}
+        {view === 'sign-up' && <SignUpCard />}
+      </div>
     </div>
   )
 }
